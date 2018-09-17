7154312ad8511fa5bd941ddd84162ed7bdf289e1
Diego Berrocal <cestdiego@gmail.com>
2018-09-17 14:02:54 -0400

    Add js behavior for selectAll

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,1 +1,11 @@
 import url from './dashboard.less';
+
+window.addEventListener('load', () => {
+    const selectAll = document.querySelector('#select-all');
+    console.log({ selectAll });
+
+    const checkboxes = document.querySelectorAll('.employee-table-row input[type="checkbox"]');
+    selectAll.addEventListener('click', () => {
+        checkboxes.forEach(checkbox => checkbox.checked = selectAll.checked);
+    });
+});
