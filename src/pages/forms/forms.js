afa6aba4c71db2377912a5b567b9e8288619147e
Diego Berrocal <cestdiego@gmail.com>
2018-10-02 22:30:51 -0400

    Add initial form page and set the date picker library in place

--- src/pages/forms/forms.js
+++ src/pages/forms/forms.js
@@ -1,3 +1,13 @@
 import url from './forms.less';
 
-console.log('lol')
+$(document).ready(function() {
+    $('input[name="birthday"]').daterangepicker({
+        singleDatePicker: true,
+        showDropdowns: true,
+        minYear: 1901,
+        maxYear: parseInt(moment().format('YYYY'),10),
+        minDate: moment().subtract(100, 'years').format(),
+        maxDate: moment().format()
+
+    });
+});
