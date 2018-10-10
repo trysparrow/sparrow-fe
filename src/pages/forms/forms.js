cb5ab46dc6a588d8ad4bf687880cb601dd3556ad
Diego Berrocal <cestdiego@gmail.com>
2018-10-10 11:19:51 -0400

    Add forms for second page

--- src/pages/forms/forms.js
+++ src/pages/forms/forms.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
         maxDate: moment().format()
 
     });
+
+    // For future proofing let's create our own javascript in Plain JS
+    const clearCollapsedForms = () => {
+        const collapsedContainerSelector = 'form.form .collapse';
+        const collapsedElements = document.querySelectorAll(`${collapsedContainerSelector} input, ${collapsedContainerSelector} select, ${collapsedContainerSelector} textarea`);
+        collapsedElements.forEach(el => {
+            el.required = false;
+            el.checked = false;
+            el.value = null;
+        });
+    };
 });
